aa107dd8ae6a9679b8bf72befd2c773c376ab112
Victor Gordian <victorgordian@hotmail.com>
2025-04-09 18:02:37 -0700

    End of video 3: Database finished.

---
+++ app/src/main/java/com/example/gymlog/Database/GymLogDAO.java
@@ -0,0 +1,21 @@
+package com.example.gymlog.Database;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import com.example.gymlog.Database.entities.GymLog;
+
+import java.util.ArrayList;
+
+
+//queries
+@Dao
+public interface GymLogDAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(GymLog gymLog);
+
+    @Query("Select * from " + GymLogDatabase.GYM_LOG_TABLE)
+    ArrayList<GymLog> getAllRecords();
+}
